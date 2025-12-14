ae640d4990016f86603dd314382748dd1df242e8
Ch. Vamshi Krishna Babu <156058457+VKB2005@users.noreply.github.com>
2025-12-14 10:17:04 +0530

    Add files via upload

---
+++ download_seizure_data.sh
@@ -0,0 +1,287 @@
+#!/bin/bash
+echo 'Starting download...'
+
+mkdir -p chb01
+wget -c -N -P chb01 https://physionet.org/files/chbmit/1.0.0/chb01/chb01_03.edf
+mkdir -p chb01
+wget -c -N -P chb01 https://physionet.org/files/chbmit/1.0.0/chb01/chb01_04.edf
+mkdir -p chb01
+wget -c -N -P chb01 https://physionet.org/files/chbmit/1.0.0/chb01/chb01_15.edf
+mkdir -p chb01
+wget -c -N -P chb01 https://physionet.org/files/chbmit/1.0.0/chb01/chb01_16.edf
+mkdir -p chb01
+wget -c -N -P chb01 https://physionet.org/files/chbmit/1.0.0/chb01/chb01_18.edf
+mkdir -p chb01
+wget -c -N -P chb01 https://physionet.org/files/chbmit/1.0.0/chb01/chb01_21.edf
+mkdir -p chb01
+wget -c -N -P chb01 https://physionet.org/files/chbmit/1.0.0/chb01/chb01_26.edf
+mkdir -p chb02
+wget -c -N -P chb02 https://physionet.org/files/chbmit/1.0.0/chb02/chb02_16+.edf
+mkdir -p chb02
+wget -c -N -P chb02 https://physionet.org/files/chbmit/1.0.0/chb02/chb02_16.edf
+mkdir -p chb02
+wget -c -N -P chb02 https://physionet.org/files/chbmit/1.0.0/chb02/chb02_19.edf
+mkdir -p chb03
+wget -c -N -P chb03 https://physionet.org/files/chbmit/1.0.0/chb03/chb03_01.edf
+mkdir -p chb03
+wget -c -N -P chb03 https://physionet.org/files/chbmit/1.0.0/chb03/chb03_02.edf
+mkdir -p chb03
+wget -c -N -P chb03 https://physionet.org/files/chbmit/1.0.0/chb03/chb03_03.edf
+mkdir -p chb03
+wget -c -N -P chb03 https://physionet.org/files/chbmit/1.0.0/chb03/chb03_04.edf
+mkdir -p chb03
+wget -c -N -P chb03 https://physionet.org/files/chbmit/1.0.0/chb03/chb03_34.edf
+mkdir -p chb03
+wget -c -N -P chb03 https://physionet.org/files/chbmit/1.0.0/chb03/chb03_35.edf
+mkdir -p chb03
+wget -c -N -P chb03 https://physionet.org/files/chbmit/1.0.0/chb03/chb03_36.edf
+mkdir -p chb04
+wget -c -N -P chb04 https://physionet.org/files/chbmit/1.0.0/chb04/chb04_05.edf
+mkdir -p chb04
+wget -c -N -P chb04 https://physionet.org/files/chbmit/1.0.0/chb04/chb04_08.edf
+mkdir -p chb04
+wget -c -N -P chb04 https://physionet.org/files/chbmit/1.0.0/chb04/chb04_28.edf
+mkdir -p chb05
+wget -c -N -P chb05 https://physionet.org/files/chbmit/1.0.0/chb05/chb05_06.edf
+mkdir -p chb05
+wget -c -N -P chb05 https://physionet.org/files/chbmit/1.0.0/chb05/chb05_13.edf
+mkdir -p chb05
+wget -c -N -P chb05 https://physionet.org/files/chbmit/1.0.0/chb05/chb05_16.edf
+mkdir -p chb05
+wget -c -N -P chb05 https://physionet.org/files/chbmit/1.0.0/chb05/chb05_17.edf
+mkdir -p chb05
+wget -c -N -P chb05 https://physionet.org/files/chbmit/1.0.0/chb05/chb05_22.edf
+mkdir -p chb06
+wget -c -N -P chb06 https://physionet.org/files/chbmit/1.0.0/chb06/chb06_01.edf
+mkdir -p chb06
+wget -c -N -P chb06 https://physionet.org/files/chbmit/1.0.0/chb06/chb06_04.edf
+mkdir -p chb06
+wget -c -N -P chb06 https://physionet.org/files/chbmit/1.0.0/chb06/chb06_09.edf
+mkdir -p chb06
+wget -c -N -P chb06 https://physionet.org/files/chbmit/1.0.0/chb06/chb06_10.edf
+mkdir -p chb06
+wget -c -N -P chb06 https://physionet.org/files/chbmit/1.0.0/chb06/chb06_13.edf
+mkdir -p chb06
+wget -c -N -P chb06 https://physionet.org/files/chbmit/1.0.0/chb06/chb06_18.edf
+mkdir -p chb06
+wget -c -N -P chb06 https://physionet.org/files/chbmit/1.0.0/chb06/chb06_24.edf
+mkdir -p chb07
+wget -c -N -P chb07 https://physionet.org/files/chbmit/1.0.0/chb07/chb07_12.edf
+mkdir -p chb07
+wget -c -N -P chb07 https://physionet.org/files/chbmit/1.0.0/chb07/chb07_13.edf
+mkdir -p chb07
+wget -c -N -P chb07 https://physionet.org/files/chbmit/1.0.0/chb07/chb07_18.edf
+mkdir -p chb08
+wget -c -N -P chb08 https://physionet.org/files/chbmit/1.0.0/chb08/chb08_02.edf
+mkdir -p chb08
+wget -c -N -P chb08 https://physionet.org/files/chbmit/1.0.0/chb08/chb08_05.edf
+mkdir -p chb08
+wget -c -N -P chb08 https://physionet.org/files/chbmit/1.0.0/chb08/chb08_11.edf
+mkdir -p chb08
+wget -c -N -P chb08 https://physionet.org/files/chbmit/1.0.0/chb08/chb08_13.edf
+mkdir -p chb08
+wget -c -N -P chb08 https://physionet.org/files/chbmit/1.0.0/chb08/chb08_21.edf
+mkdir -p chb09
+wget -c -N -P chb09 https://physionet.org/files/chbmit/1.0.0/chb09/chb09_06.edf
+mkdir -p chb09
+wget -c -N -P chb09 https://physionet.org/files/chbmit/1.0.0/chb09/chb09_08.edf
+mkdir -p chb09
+wget -c -N -P chb09 https://physionet.org/files/chbmit/1.0.0/chb09/chb09_19.edf
+mkdir -p chb10
+wget -c -N -P chb10 https://physionet.org/files/chbmit/1.0.0/chb10/chb10_12.edf
+mkdir -p chb10
+wget -c -N -P chb10 https://physionet.org/files/chbmit/1.0.0/chb10/chb10_20.edf
+mkdir -p chb10
+wget -c -N -P chb10 https://physionet.org/files/chbmit/1.0.0/chb10/chb10_27.edf
+mkdir -p chb10
+wget -c -N -P chb10 https://physionet.org/files/chbmit/1.0.0/chb10/chb10_30.edf
+mkdir -p chb10
+wget -c -N -P chb10 https://physionet.org/files/chbmit/1.0.0/chb10/chb10_31.edf
+mkdir -p chb10
+wget -c -N -P chb10 https://physionet.org/files/chbmit/1.0.0/chb10/chb10_38.edf
+mkdir -p chb10
+wget -c -N -P chb10 https://physionet.org/files/chbmit/1.0.0/chb10/chb10_89.edf
+mkdir -p chb11
+wget -c -N -P chb11 https://physionet.org/files/chbmit/1.0.0/chb11/chb11_82.edf
+mkdir -p chb11
+wget -c -N -P chb11 https://physionet.org/files/chbmit/1.0.0/chb11/chb11_92.edf
+mkdir -p chb11
+wget -c -N -P chb11 https://physionet.org/files/chbmit/1.0.0/chb11/chb11_99.edf
+mkdir -p chb12
+wget -c -N -P chb12 https://physionet.org/files/chbmit/1.0.0/chb12/chb12_06.edf
+mkdir -p chb12
+wget -c -N -P chb12 https://physionet.org/files/chbmit/1.0.0/chb12/chb12_08.edf
+mkdir -p chb12
+wget -c -N -P chb12 https://physionet.org/files/chbmit/1.0.0/chb12/chb12_09.edf
+mkdir -p chb12
+wget -c -N -P chb12 https://physionet.org/files/chbmit/1.0.0/chb12/chb12_10.edf
+mkdir -p chb12
+wget -c -N -P chb12 https://physionet.org/files/chbmit/1.0.0/chb12/chb12_11.edf
+mkdir -p chb12
+wget -c -N -P chb12 https://physionet.org/files/chbmit/1.0.0/chb12/chb12_23.edf
+mkdir -p chb12
+wget -c -N -P chb12 https://physionet.org/files/chbmit/1.0.0/chb12/chb12_27.edf
+mkdir -p chb12
+wget -c -N -P chb12 https://physionet.org/files/chbmit/1.0.0/chb12/chb12_28.edf
+mkdir -p chb12
+wget -c -N -P chb12 https://physionet.org/files/chbmit/1.0.0/chb12/chb12_29.edf
+mkdir -p chb12
+wget -c -N -P chb12 https://physionet.org/files/chbmit/1.0.0/chb12/chb12_33.edf
+mkdir -p chb12
+wget -c -N -P chb12 https://physionet.org/files/chbmit/1.0.0/chb12/chb12_36.edf
+mkdir -p chb12
+wget -c -N -P chb12 https://physionet.org/files/chbmit/1.0.0/chb12/chb12_38.edf
+mkdir -p chb12
+wget -c -N -P chb12 https://physionet.org/files/chbmit/1.0.0/chb12/chb12_42.edf
+mkdir -p chb13
+wget -c -N -P chb13 https://physionet.org/files/chbmit/1.0.0/chb13/chb13_19.edf
+mkdir -p chb13
+wget -c -N -P chb13 https://physionet.org/files/chbmit/1.0.0/chb13/chb13_21.edf
+mkdir -p chb13
+wget -c -N -P chb13 https://physionet.org/files/chbmit/1.0.0/chb13/chb13_40.edf
+mkdir -p chb13
+wget -c -N -P chb13 https://physionet.org/files/chbmit/1.0.0/chb13/chb13_55.edf
+mkdir -p chb13
+wget -c -N -P chb13 https://physionet.org/files/chbmit/1.0.0/chb13/chb13_58.edf
+mkdir -p chb13
+wget -c -N -P chb13 https://physionet.org/files/chbmit/1.0.0/chb13/chb13_59.edf
+mkdir -p chb13
+wget -c -N -P chb13 https://physionet.org/files/chbmit/1.0.0/chb13/chb13_60.edf
+mkdir -p chb13
+wget -c -N -P chb13 https://physionet.org/files/chbmit/1.0.0/chb13/chb13_62.edf
+mkdir -p chb14
+wget -c -N -P chb14 https://physionet.org/files/chbmit/1.0.0/chb14/chb14_03.edf
+mkdir -p chb14
+wget -c -N -P chb14 https://physionet.org/files/chbmit/1.0.0/chb14/chb14_04.edf
+mkdir -p chb14
+wget -c -N -P chb14 https://physionet.org/files/chbmit/1.0.0/chb14/chb14_06.edf
+mkdir -p chb14
+wget -c -N -P chb14 https://physionet.org/files/chbmit/1.0.0/chb14/chb14_11.edf
+mkdir -p chb14
+wget -c -N -P chb14 https://physionet.org/files/chbmit/1.0.0/chb14/chb14_17.edf
+mkdir -p chb14
+wget -c -N -P chb14 https://physionet.org/files/chbmit/1.0.0/chb14/chb14_18.edf
+mkdir -p chb14
+wget -c -N -P chb14 https://physionet.org/files/chbmit/1.0.0/chb14/chb14_27.edf
+mkdir -p chb15
+wget -c -N -P chb15 https://physionet.org/files/chbmit/1.0.0/chb15/chb15_06.edf
+mkdir -p chb15
+wget -c -N -P chb15 https://physionet.org/files/chbmit/1.0.0/chb15/chb15_10.edf
+mkdir -p chb15
+wget -c -N -P chb15 https://physionet.org/files/chbmit/1.0.0/chb15/chb15_15.edf
+mkdir -p chb15
+wget -c -N -P chb15 https://physionet.org/files/chbmit/1.0.0/chb15/chb15_17.edf
+mkdir -p chb15
+wget -c -N -P chb15 https://physionet.org/files/chbmit/1.0.0/chb15/chb15_20.edf
+mkdir -p chb15
+wget -c -N -P chb15 https://physionet.org/files/chbmit/1.0.0/chb15/chb15_22.edf
+mkdir -p chb15
+wget -c -N -P chb15 https://physionet.org/files/chbmit/1.0.0/chb15/chb15_28.edf
+mkdir -p chb15
+wget -c -N -P chb15 https://physionet.org/files/chbmit/1.0.0/chb15/chb15_31.edf
+mkdir -p chb15
+wget -c -N -P chb15 https://physionet.org/files/chbmit/1.0.0/chb15/chb15_40.edf
+mkdir -p chb15
+wget -c -N -P chb15 https://physionet.org/files/chbmit/1.0.0/chb15/chb15_46.edf
+mkdir -p chb15
+wget -c -N -P chb15 https://physionet.org/files/chbmit/1.0.0/chb15/chb15_49.edf
+mkdir -p chb15
+wget -c -N -P chb15 https://physionet.org/files/chbmit/1.0.0/chb15/chb15_52.edf
+mkdir -p chb15
+wget -c -N -P chb15 https://physionet.org/files/chbmit/1.0.0/chb15/chb15_54.edf
+mkdir -p chb15
+wget -c -N -P chb15 https://physionet.org/files/chbmit/1.0.0/chb15/chb15_62.edf
+mkdir -p chb16
+wget -c -N -P chb16 https://physionet.org/files/chbmit/1.0.0/chb16/chb16_10.edf
+mkdir -p chb16
+wget -c -N -P chb16 https://physionet.org/files/chbmit/1.0.0/chb16/chb16_11.edf
+mkdir -p chb16
+wget -c -N -P chb16 https://physionet.org/files/chbmit/1.0.0/chb16/chb16_14.edf
+mkdir -p chb16
+wget -c -N -P chb16 https://physionet.org/files/chbmit/1.0.0/chb16/chb16_16.edf
+mkdir -p chb16
+wget -c -N -P chb16 https://physionet.org/files/chbmit/1.0.0/chb16/chb16_17.edf
+mkdir -p chb16
+wget -c -N -P chb16 https://physionet.org/files/chbmit/1.0.0/chb16/chb16_18.edf
+mkdir -p chb17
+wget -c -N -P chb17 https://physionet.org/files/chbmit/1.0.0/chb17/chb17a_03.edf
+mkdir -p chb17
+wget -c -N -P chb17 https://physionet.org/files/chbmit/1.0.0/chb17/chb17a_04.edf
+mkdir -p chb17
+wget -c -N -P chb17 https://physionet.org/files/chbmit/1.0.0/chb17/chb17b_63.edf
+mkdir -p chb18
+wget -c -N -P chb18 https://physionet.org/files/chbmit/1.0.0/chb18/chb18_29.edf
+mkdir -p chb18
+wget -c -N -P chb18 https://physionet.org/files/chbmit/1.0.0/chb18/chb18_30.edf
+mkdir -p chb18
+wget -c -N -P chb18 https://physionet.org/files/chbmit/1.0.0/chb18/chb18_31.edf
+mkdir -p chb18
+wget -c -N -P chb18 https://physionet.org/files/chbmit/1.0.0/chb18/chb18_32.edf
+mkdir -p chb18
+wget -c -N -P chb18 https://physionet.org/files/chbmit/1.0.0/chb18/chb18_35.edf
+mkdir -p chb18
+wget -c -N -P chb18 https://physionet.org/files/chbmit/1.0.0/chb18/chb18_36.edf
+mkdir -p chb19
+wget -c -N -P chb19 https://physionet.org/files/chbmit/1.0.0/chb19/chb19_28.edf
+mkdir -p chb19
+wget -c -N -P chb19 https://physionet.org/files/chbmit/1.0.0/chb19/chb19_29.edf
+mkdir -p chb19
+wget -c -N -P chb19 https://physionet.org/files/chbmit/1.0.0/chb19/chb19_30.edf
+mkdir -p chb20
+wget -c -N -P chb20 https://physionet.org/files/chbmit/1.0.0/chb20/chb20_12.edf
+mkdir -p chb20
+wget -c -N -P chb20 https://physionet.org/files/chbmit/1.0.0/chb20/chb20_13.edf
+mkdir -p chb20
+wget -c -N -P chb20 https://physionet.org/files/chbmit/1.0.0/chb20/chb20_14.edf
+mkdir -p chb20
+wget -c -N -P chb20 https://physionet.org/files/chbmit/1.0.0/chb20/chb20_15.edf
+mkdir -p chb20
+wget -c -N -P chb20 https://physionet.org/files/chbmit/1.0.0/chb20/chb20_16.edf
+mkdir -p chb20
+wget -c -N -P chb20 https://physionet.org/files/chbmit/1.0.0/chb20/chb20_68.edf
+mkdir -p chb21
+wget -c -N -P chb21 https://physionet.org/files/chbmit/1.0.0/chb21/chb21_19.edf
+mkdir -p chb21
+wget -c -N -P chb21 https://physionet.org/files/chbmit/1.0.0/chb21/chb21_20.edf
+mkdir -p chb21
+wget -c -N -P chb21 https://physionet.org/files/chbmit/1.0.0/chb21/chb21_21.edf
+mkdir -p chb21
+wget -c -N -P chb21 https://physionet.org/files/chbmit/1.0.0/chb21/chb21_22.edf
+mkdir -p chb22
+wget -c -N -P chb22 https://physionet.org/files/chbmit/1.0.0/chb22/chb22_20.edf
+mkdir -p chb22
+wget -c -N -P chb22 https://physionet.org/files/chbmit/1.0.0/chb22/chb22_25.edf
+mkdir -p chb22
+wget -c -N -P chb22 https://physionet.org/files/chbmit/1.0.0/chb22/chb22_38.edf
+mkdir -p chb23
+wget -c -N -P chb23 https://physionet.org/files/chbmit/1.0.0/chb23/chb23_06.edf
+mkdir -p chb23
+wget -c -N -P chb23 https://physionet.org/files/chbmit/1.0.0/chb23/chb23_08.edf
+mkdir -p chb23
+wget -c -N -P chb23 https://physionet.org/files/chbmit/1.0.0/chb23/chb23_09.edf
+mkdir -p chb24
+wget -c -N -P chb24 https://physionet.org/files/chbmit/1.0.0/chb24/chb24_01.edf
+mkdir -p chb24
+wget -c -N -P chb24 https://physionet.org/files/chbmit/1.0.0/chb24/chb24_03.edf
+mkdir -p chb24
+wget -c -N -P chb24 https://physionet.org/files/chbmit/1.0.0/chb24/chb24_04.edf
+mkdir -p chb24
+wget -c -N -P chb24 https://physionet.org/files/chbmit/1.0.0/chb24/chb24_06.edf
+mkdir -p chb24
+wget -c -N -P chb24 https://physionet.org/files/chbmit/1.0.0/chb24/chb24_07.edf
+mkdir -p chb24
+wget -c -N -P chb24 https://physionet.org/files/chbmit/1.0.0/chb24/chb24_09.edf
+mkdir -p chb24
+wget -c -N -P chb24 https://physionet.org/files/chbmit/1.0.0/chb24/chb24_11.edf
+mkdir -p chb24
+wget -c -N -P chb24 https://physionet.org/files/chbmit/1.0.0/chb24/chb24_13.edf
+mkdir -p chb24
+wget -c -N -P chb24 https://physionet.org/files/chbmit/1.0.0/chb24/chb24_14.edf
+mkdir -p chb24
+wget -c -N -P chb24 https://physionet.org/files/chbmit/1.0.0/chb24/chb24_15.edf
+mkdir -p chb24
+wget -c -N -P chb24 https://physionet.org/files/chbmit/1.0.0/chb24/chb24_17.edf
+mkdir -p chb24
+wget -c -N -P chb24 https://physionet.org/files/chbmit/1.0.0/chb24/chb24_21.edf
+
+echo 'Download complete!'
